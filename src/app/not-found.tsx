3265dcdea70b0a3b0e4fcbc7e786f8814eb8199d
Jayasriraam-s <jayasriraam27@gmail.com>
2024-04-08 15:24:32 +0530

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <section>
+      <h1 className="text-[3rem] text-center font-bold">Not Found...</h1>
+      <p>please try again later...</p>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,285 @@
-import Image from "next/image";
+"use client";
+
+import Darkmode from "@/components/darkmode";
+import { useState, FormEvent } from "react";
+import { Input } from "@/components/ui/input";
+import "./styles.css";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Book {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    description: string;
+    publishedDate: string;
+    categories: string[];
+    imageLinks: {
+      thumbnail: string;
+    };
+    previewLink: string;
+  };
+  saleInfo: {
+    buyLink: string;
+  };
+  accessInfo: {
+    pdf: {
+      acsTokenLink: string;
+    };
+    epub: {
+      acsTokenLink: string;
+    };
+  };
+}
 
 export default function Home() {
+  const [query, setQuery] = useState<string>("");
+  const [books, setBooks] = useState<Book[]>([]);
+  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const maxResults = 30;
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+
+      setTimeout(() => {
+        console.log(data.items);
+        setBooks(data.items);
+        setLoading(false);
+      }, 5000);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearch = e.target.value;
+    setQuery(newSearch);
+  };
+
+  const handleSearch = (book: Book) => {
+    setSelectedBook(book);
+  };
+
+  const handleClose = () => {
+    setSelectedBook(null);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section>
+      <header>
+        <div className="flex justify-evenly items-center gap-5 max-w-[1300px] mx-auto mt-7">
+          <h1 className="text-[20px]">
+            <span className="font-bold text-[35px]">B</span>ook Search
+          </h1>
+          <Darkmode />
         </div>
+      </header>
+
+      <div className="container">
+        <form onSubmit={handleSubmit} className="form" name="books_search">
+          <div className="inputGroup flex justify-center gap-3 items-center max-w-[1000px] mx-auto my-0 mt-8">
+            <Input
+              type="text"
+              value={query}
+              onChange={handleChange}
+              placeholder="Search the Book"
+              className="Input_field"
+              autoComplete="off"
+              autoFocus
+            />
+            <Button type="submit" className="btn live">
+              Search
+            </Button>
+          </div>
+        </form>
+        {loading && (
+          <div className="hourglassBackground">
+            <div className="hourglassContainer">
+              <div className="hourglassCurves"></div>
+              <div className="hourglassCapTop"></div>
+              <div className="hourglassGlassTop"></div>
+              <div className="hourglassSand"></div>
+              <div className="hourglassSandStream"></div>
+              <div className="hourglassCapBottom"></div>
+              <div className="hourglassGlass"></div>
+            </div>
+          </div>
+        )}
+
+        <div className="flex flex-wrap max-w-[1200px] justify-center gap-12 mx-auto my-0 mt-10 pb-10">
+          {books.map((book) => (
+            <Card>
+              <div key={book.id} className="book-container">
+                <CardHeader>
+                  <img
+                    src={
+                      book.volumeInfo.imageLinks
+                        ? book.volumeInfo.imageLinks.thumbnail
+                        : "https://placehold.co/400x300/orange/white"
+                    }
+                    alt="Book cover"
+                    onClick={() => handleSearch(book)}
+                    className="book-cover"
+                  />
+                </CardHeader>
+                <CardTitle>
+                  <h2 className="book-title mb-6">{book.volumeInfo.title}</h2>
+                </CardTitle>
+                <CardContent>
+                  <p className="flex justify-center items-center gap-5 text-center">
+                    <span>Author(s)</span>
+                    {book.volumeInfo.authors
+                      ? book.volumeInfo.authors.join(", ")
+                      : "Unknown"}
+                  </p>
+                  <p className="flex justify-center items-center gap-5 text-center mt-3">
+                    <span>Publisher</span>
+                    {book.volumeInfo.publisher
+                      ? book.volumeInfo.publisher
+                      : "Unknown"}
+                  </p>
+                </CardContent>
+                <CardDescription>
+                  <div className="flex justify-center items-center gap-5 flex-col">
+                    {book.saleInfo.buyLink && (
+                      <div>
+                        <a
+                          href={book.saleInfo.buyLink}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="gal-1"
+                        >
+                          <Button type="button" className="btn-2">
+                            Buy
+                          </Button>
+                        </a>
+                      </div>
+                    )}
+                    {book.accessInfo.pdf.acsTokenLink && (
+                      <div>
+                        <a
+                          href={book.accessInfo.pdf.acsTokenLink}
+                          className="main-3 btn-2"
+                          download={book.volumeInfo.title}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Download PDF
+                        </a>
+                      </div>
+                    )}
+                    {book.accessInfo.epub.acsTokenLink && (
+                      <div>
+                        <a
+                          href={book.accessInfo.epub.acsTokenLink}
+                          className="main-3 btn-2"
+                          download={book.volumeInfo.title}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Download EPUB
+                        </a>
+                      </div>
+                    )}
+                  </div>
+                </CardDescription>
+              </div>
+            </Card>
+          ))}
+        </div>
+
+        {selectedBook && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={handleClose}>
+                &times;
+              </span>
+              <div className="flex justify-center items-center gap-5 flex-col">
+                <img
+                  src={
+                    selectedBook.volumeInfo.imageLinks
+                      ? selectedBook.volumeInfo.imageLinks.thumbnail
+                      : "https://placehold.co/150x100/orange/white"
+                  }
+                  alt="Book cover"
+                  className="book-cover"
+                />
+                <h2>{selectedBook.volumeInfo.title}</h2>
+                <p className="text-black">
+                  Author(s):{" "}
+                  {selectedBook.volumeInfo.authors
+                    ? selectedBook.volumeInfo.authors.join(", ")
+                    : "Unknown"}
+                </p>
+                <p className="text-black">
+                  Publisher:{" "}
+                  {selectedBook.volumeInfo.publisher
+                    ? selectedBook.volumeInfo.publisher
+                    : "Unknown"}
+                </p>
+                <p className="text-black">
+                  categories:{" "}
+                  {selectedBook.volumeInfo.categories
+                    ? selectedBook.volumeInfo.categories
+                    : "Unknown"}
+                </p>
+                <p className="text-black">
+                  Published Date:{" "}
+                  {selectedBook.volumeInfo.publishedDate
+                    ? selectedBook.volumeInfo.publishedDate
+                    : "DD-MM-YYYY"}
+                </p>
+                <p className="text-black">
+                  Description:{" "}
+                  {selectedBook.volumeInfo.description
+                    ? selectedBook.volumeInfo.description.length > 150
+                      ? selectedBook.volumeInfo.description.substring(0, 150) +
+                        "..."
+                      : selectedBook.volumeInfo.description
+                    : "No Data Found in this book"}
+                </p>
+                <div className="flex justify-center items-center gap-5">
+                  {selectedBook.volumeInfo.previewLink && (
+                    <div>
+                      <a
+                        href={selectedBook.volumeInfo.previewLink}
+                        className="gal-1"
+                      >
+                        <Button
+                          type="button"
+                          className="p-4 hover:bg-black hover:text-white rounded"
+                        >
+                          Preview The Book
+                        </Button>
+                      </a>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
